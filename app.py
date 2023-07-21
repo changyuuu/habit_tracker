c430b911feb197e2aaa419b9da260683f12479a8
Roger <roger0981651123@gmail.com>
2023-07-21 20:58:33 +0800

    bulid: saving habits and render

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+habits = ["Test habit"]
+
+
 @app.route("/")
 def index():
-  return render_template("index.html", title="Home")
+  return render_template("index.html", habits=habits,  title="Home")
 
 @app.route("/add", methods=["GET","POST"])
 def add_habit():
+  
+  if request.method == "POST":
+    habit = request.form.get("habit")
+    habits.append(habit)
+    return redirect(url_for(request.url))
+    
   return render_template("add_habit.html",title="Add Habit")
 
 
