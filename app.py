ab467fcb9096f25b8a1dd4db660c68b94805aa16
Roger <roger0981651123@gmail.com>
2023-07-22 21:29:22 +0800

    bulid: Adding date negivation

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 from flask import Flask, render_template, redirect, url_for, request
+import datetime
 
 app = Flask(__name__)
 
 habits = ["Test habit"]
 
+@app.context_processor
+def add_calc_date_range():
+  def date_range(start: datetime.date):
+    dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+    return dates
+  
+  return {"date_range": date_range}
 
 @app.route("/")
 def index():
-  return render_template("index.html", habits=habits,  title="Home")
+  date_str = request.args.get('date')
+
+  if date_str:
+    selected_date = datetime.date.fromisoformat(date_str)
+  else:
+    selected_date = datetime.date.today()
+
+  return render_template("index.html", habits=habits,selected_date=selected_date, title="Home")
 
 @app.route("/add", methods=["GET","POST"])
 def add_habit():
   
   if request.method == "POST":
-    habit = request.form.get("habit")
-    habits.append(habit)
+    habits.append(request.form.get("habit"))
     
-  return render_template("add_habit.html",title="Add Habit")
+  return render_template("add_habit.html",selected_date=datetime.date.today(), title="Add Habit")
 
 
 if __name__ == "__main__":
